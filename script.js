169d977501a5a558a78fe86be8cc84fd0851bb07
palmarizarte <palmarizarte@gmail.com>
2025-02-06 02:25:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const btnMenu = document.querySelector(".btn");
 const list = document.querySelector(".list");
+const navLinks = document.querySelectorAll(".list li a");
 
-btnMenu.addEventListener("click",()=>{
+btnMenu.addEventListener("click", () => {
     list.classList.toggle("list-toggle");
     btnMenu.classList.toggle("btn-toggle");
-})
+});
+
+// Cerrar el menú al hacer clic en un enlace
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        list.classList.remove("list-toggle");
+        btnMenu.classList.remove("btn-toggle");
+    });
+});
